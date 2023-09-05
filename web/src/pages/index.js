cb935d3f57ff2eb2f5aef866370aabdc63392509
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-09-05 23:50:56 +0500

    Update index.js

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -12,7 +12,7 @@ import Testimonials from "../components/Repeating/Testimonials";
 import CallToAction from "../components/Repeating/CTA";
 import ButtonSolid from "../components/Button/ButtonSolid";
 import ButtonUnderline from "../components/Button/ButtonUnderline";
-
+import Analytics from "../components/Analytics/Analytics.jsx";
 const Page = ({ data }) => {
   const heroFullWidthImages = [
     getImage(data.heroDesktop.childImageSharp.gatsbyImageData),
@@ -69,7 +69,7 @@ const Page = ({ data }) => {
         openGraphImage={data.openGraphImage.publicURL}
         twitterOpenGraphImage={data.twitterOpenGraphImage.publicURL}
       />
-
+<Analytics />
       <HeroFullWidth
         backgroundImages={heroFullWidthImages}
         backgroundPosition="50% 25%"
