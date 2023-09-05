73419640b09d3e5a06c5e6e0bf03ff5499078494
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-09-05 23:51:25 +0500

    Create Analytics.jsx

---
+++ web/src/components/Analytics/Analytics.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Analytics() {
+  return (
+    <div>
+      <a title="Web Analytics" href="https://statcounter.com/" target="_blank">
+        <img
+          src="https://c.statcounter.com/12916898/0/9e9b5e41/1/"
+          alt="Web Analytics"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default Analytics;
