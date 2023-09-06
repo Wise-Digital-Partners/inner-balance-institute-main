477e8125544a3be8e7d923600a507e7eb70fc1f7
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-09-06 10:05:49 +0500

    Update Analytics.jsx

--- web/src/components/Analytics/Analytics.jsx
+++ web/src/components/Analytics/Analytics.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 function Analytics() {
   return (
-    <div>
-      <a title="Web Analytics" href="https://statcounter.com/" target="_blank">
-        <img
-          src="https://c.statcounter.com/12916898/0/9e9b5e41/1/"
-          alt="Web Analytics"
-        />
-      </a>
-    </div>
+   <div>
+     <a title="hit counter" href="https://statcounter.com/" target="_blank">
+       <img src="https://c.statcounter.com/12916898/0/9e9b5e41/1/" alt="hit counter">
+       </a>
+     </div>
   );
 }
 
